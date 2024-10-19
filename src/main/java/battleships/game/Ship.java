91acbea1cf4d05340a6ca443ad5e0296b2cffe01
kloggenberg023 <kloggenberg023@student.wethinkcode.co.za>
2024-10-19 13:15:55 +0200

    Adding Ship class

---
+++ src/main/java/battleships/game/Ship.java
@@ -0,0 +1,14 @@
+package battleships.game;
+
+public abstract class Ship {
+    private int shipHealth;
+    private int shipSpace;
+
+    public int getShipHealth() {
+        return shipHealth;
+    }
+
+    public int getShipSpace() {
+        return shipSpace;
+    }
+}
