53c6351ef7dee4b4a6a5f8961c9154c7b383bb2d
kloggenberg023 <kloggenberg023@student.wethinkcode.co.za>
2024-10-19 14:21:12 +0200

    Updating Main class

--- src/main/java/battleships/game/Main.java
+++ src/main/java/battleships/game/Main.java
@@ -1,7 +1,11 @@
 package battleships.game;
 
+import battleships.game.ships.Carrier;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Carrier ship1 = new Carrier();
+
+        System.out.println(ship1);
     }
 }
